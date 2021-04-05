323e1a4d0c68760d5f9ce46c486a67a3b9a46b79
vannhatufl1998 <vannhat.nielsen@gmail.com>
2021-04-05 11:03:31 +0700

    Nhật commit
    Nhật đưa Web demo

---
+++ ASP.NET_MVC5-ShoppingCart-master/ShoppingCart_ASP.NET_MVC5/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using ShoppingCart_ASP.NET_MVC5.Dao;
+using ShoppingCart_ASP.NET_MVC5.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ShoppingCart_ASP.NET_MVC5.Controllers
+{
+    [SessionExpire]
+    public class HomeController : Controller
+    {
+       
+        public ActionResult Home()
+        {
+            List<Product> prolist = ProductDetails.callme();
+            ViewData["pl"] = prolist;
+            string sessionId = SessionManagement.CreateSession(int.Parse(Request.Cookies["customer_id"].Value));
+            ViewData["sessionId"] = sessionId;
+            ViewBag.a = 1;
+            ViewBag.customer = int.Parse(Request.Cookies["customer_id"].Value);
+            ViewBag.Count = int.Parse(Request.Cookies["Count"].Value);
+            ViewBag.firstname = Request.Cookies["firstname"].Value;
+            return View();
+
+        }
+
+
+        
+
+
+    }
+}
---
+++ ASP.NET_MVC5-ShoppingCart-master/ShoppingCart_ASP.NET_MVC5/Controllers/SearchController.cs
@@ -0,0 +1,56 @@
+﻿using ShoppingCart_ASP.NET_MVC5.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ShoppingCart_ASP.NET_MVC5.Dao;
+
+namespace ShoppingCart_ASP.NET_MVC5.Controllers
+{
+    [SessionExpire]
+    public class SearchController : Controller
+    {
+        
+        public ActionResult Partial(string search)
+
+        {
+            if(search == "")
+            {
+                ViewBag.a = "Please enter a search item in the search box..";
+            }
+            List<Product> pro = new List<Product>();
+            using (SqlConnection conn = new SqlConnection("Server=.; Database=ShoppingCartT4; Integrated Security=true"))
+            {
+                conn.Open();
+                string sql = @"select * from Product where pro_desc like '%"+ search +"%'";
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                SqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    Product p = new Product()
+                    {
+                        pro_id = (int)reader["pro_id"],
+                        pro_name = (string)reader["pro_name"],
+                        pro_desc = (string)reader["pro_desc"],
+                        pro_price = (int)reader["pro_price"],
+                        pro_image = (string)reader["pro_image"]
+
+                    };
+                    pro.Add(p);
+                }
+            }
+            if (pro != null)
+            {
+                ViewData["partial"] = pro;
+            }
+            else
+                ViewBag.alert = "Sorry, the item is out of stock..";
+            ViewBag.Count = int.Parse(Request.Cookies["Count"].Value);
+            ViewBag.firstname = Request.Cookies["firstname"].Value;
+            ViewBag.a = 2;
+            return View("../Home/Home");
+        }
+    }
+}
